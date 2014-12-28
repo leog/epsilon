a85eae26acb3883bdefddef17b356dba10f936bc
Leonardo Giovanetti <leonardo.giovanetti@autodesk.com>
2014-12-28 16:35:28 -0200

    Changing baseUrl on RequireJS config just for gh-pages

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 require.config({
-    'baseUrl': '',
+    'baseUrl': '/epsilon',
     'paths': {
         // Bower libraries paths
         'i18n': 'lib/vendor/bower_components/requirejs-i18n/i18n',
